7456d6b642b8958793c5de5c895a721376d6b216
ethanflyn <102313981+ethanflyn@users.noreply.github.com>
2024-05-06 10:19:27 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
             <p><a href="eflyn10.w3.uvm.edu/cs1080/final">Ethan Flynn - Section A</a></p>
-            <p><a href="        .w3.uvm.edu/cs1080/final">Jack Goring - Section A</a></p>
+            <p><a href="jgoring.w3.uvm.edu/cs1080/final">Jack Goring - Section A</a></p>
 </footer>
