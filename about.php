04524a59bcfb460cd534ac591f7c942f76b5bb30
jgoring <157045784+jgoring@users.noreply.github.com>
2024-05-06 09:03:25 -0400

    Update about.php

--- about.php
+++ about.php
@@ -7,32 +7,32 @@ include 'top.php';
             <h1>The Main Events</h1>
             <section class="grid-container">
                 <section class="grid-item">
-                <img alt="Ane" src="images/ane.JPG" class="image">
+                <img alt="Athletics" src="images/olympic-athletic.jpeg" class="image">
                     <h2>Athletics</h2> 
                     <p>Athletics in the Olympics comprises track and field events, showcasing human physical prowess. Athletes compete in sprints, jumps, throws, and distance runs, embodying the Olympic values of excellence, perseverance, and international unity. This cornerstone of the Games highlights the peak of athletic achievement and the global nature of sportsmanship.</p>
                 </section>
                 <section class="grid-item">
-                <img alt="Cris" src="images/cris.jpg" class="image">
+                <img alt="Diving" src="images/olympic-diving.webp" class="image">
                     <h2>Diving</h2> 
                     <p>Diving in the Olympics is a breathtaking display of grace, skill, and precision. Athletes leap from springboards and platforms, executing intricate acrobatics in mid-air before entering the water with barely a splash. Judges evaluate the dives based on form, execution, and degree of difficulty, making it a captivating spectacle of athleticism.</p>
                 </section>
                 <section class="grid-item">
-                <img alt="Chari" src="images/chari.jpg" class="image">
+                <img alt="Rowing" src="images/olympic-rowing.jpeg" class="image">
                     <h2>Rowing</h2> 
                     <p>Rowing in the Olympics showcases teamwork, endurance, and technique. Athletes propel boats through water using oars, competing in various events like single sculls, pairs, and eights. Races are held over distances of 2,000 meters, demanding physical strength, mental toughness, and perfect synchronization. It's a thrilling display of skill and strategy on the water.</p>
                 </section>
                 <section class="grid-item">
-                <img alt="Eukene" src="images/eukene.jpg" class="image">
+                <img alt="Sateboarding" src="images/olympic-skateboarding.jpeg" class="image">
                     <h2>Skateboarding</h2> 
                     <p>Skateboarding in the Olympics is a dynamic and youthful addition, featuring two disciplines: street and park. Athletes perform tricks on ramps, rails, and obstacles, showcasing creativity, style, and technical mastery. Judged on difficulty, originality, and execution, skateboarding brings a fresh, urban energy to the Games, appealing to a new generation of fans.</p>
                 </section>
                 <section class="grid-item">
-                <img alt="Arantxa" src="images/arantxa.jpg" class="image">
+                <img alt="Surfing" src="images/olympic-surfing.jpeg" class="image">
                     <h2>Surfing</h2> 
                     <p>Surfing in the Olympics is a thrilling display of wave-riding skill and artistry. Surfers compete in the ocean, showcasing their agility, balance, and wave selection. Judged on maneuvers, speed, and flow, the sport embodies a deep connection with nature and the elements, offering a unique and captivating event to the Games.</p>
                 </section>
                 <section class="grid-item">
-                <img alt="Bettina" src="images/bettina.jpg" class="image">
+                <img alt="Weightlifting" src="images/olympic-weightlifting.jpeg" class="image">
                     <h2>Weightlifting</h2> 
                     <p>Weightlifting in the Olympics is a test of pure strength and technique. Athletes compete in two lifts: the snatch and the clean and jerk, aiming to lift the heaviest weights possible. The sport emphasizes explosive power, concentration, and precision, showcasing the limits of human strength in a thrilling and intense competition.</p>
                 </section>
